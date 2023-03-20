ffd0dd528f5a7af76df0824bf186f96dde0f3977
dev-manuel <manuel_krofficial@online.de>
2023-03-20 13:46:49 +0100

    initial commit

---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    for (var i = 0; i < details.requestHeaders.length; ++i) {
+      if (details.requestHeaders[i].name === 'User-Agent') {
+        details.requestHeaders[i].value = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.100.0';
+        break;
+      }
+    }
+    return {requestHeaders: details.requestHeaders};
+  },
+  {urls: ['https://www.bing.com/*']},
+  ['blocking', 'requestHeaders']
+);
